465c5e3732a2c25030691436b182906423665e52
chenfengami <541307194@qq.com>
2018-02-09 17:22:59 +0800

    md

--- README.md
+++ README.md
@@ -1,23 +1,42 @@
-# 表单验证器
-##使用方法
-    /**
-     * @param {验证表单信息}
-     * @param {使用方法}
-     *  var errorMsg = self.$utils.validator()([{
-     *  input: xxx, //需要验证的表单value值
-     *  rules: [{
-     *      strategy: xx, //所定义的验证方法
-     *      errorMsg: xx //错误提示信息 
-     *  }]
-     * },{
-     *  input: xxx, //需要验证的表单value值
-     *  rules: [{
-     *      strategy: xx, //所定义的验证方法
-     *      errorMsg: xx //错误提示信息 
-     *  }] 
-     * }])
-     *   if(errorMsg){
-     *     self.$vux.toast.text(errorMsg);
-     *     return;
-     *   }
-     */
+表单验证
+=======
+目前支持的验证类型
+-------
+
+. isNonEmpty 判断是否为空
+. minLength:6 最小长度限制为6位
+. maxLength:24 最大长度限制为24位
+. isEqual 是否相等验证
+. isPhone 手机号码验证
+. isEmail 邮箱验证
+. 之后会继续完善，想自己定义也可以在strategies对象里面进行匹配
+
+使用方法
+=======
+
+### 引入validator.js 传入options
+    validator().validataFunc(options) //返回的是errorMsg
+
+### options参数的具体格式
+#### 单一规则匹配
+    单一匹配的只在rules数组里面输入 一个Json对象，并匹配相应的规则和错误提示信息
+    options = [{
+        input: input.value, //需要验证表单的value值,
+        rules: [{
+            strategy: isNonEmpty, //输入验证的类型 这里为【判断是否为空】
+            errorMsg: '不能为空'
+        }]
+    }]
+#### 多规则匹配
+    多规则匹配的只在rules数组里面输入多个Json对象，并匹配相应的规则和错误提示信息
+    options = [{
+        input: input.value, //需要验证表单的value值,
+        rules: [{
+            strategy: minLength:6, //输入验证的类型 这里为【最小长度限制为:6位】
+            errorMsg: '输入的长度不能小于6位'
+        },{
+            strategy: maxLength:24, //输入验证的类型 这里为【最大长度限制为:24位】
+            errorMsg: '输入的长度不能大于24位'            
+        }]
+    }]
+    //以上对表单的限制规则为长度要在6-24个字符之间。包含6和24。
--- validator.js
+++ validator.js
@@ -1,55 +1,48 @@
-/**
-     * @param {验证表单信息}
-     * @param {使用方法}
-     *  var errorMsg = self.$utils.validator()([{
-     *  input: xxx, //需要验证的表单value值
-     *  rules: [{
-     *      strategy: xx, //所定义的验证方法
-     *      errorMsg: xx //错误提示信息 
-     *  }]
-     * },{
-     *  input: xxx, //需要验证的表单value值
-     *  rules: [{
-     *      strategy: xx, //所定义的验证方法
-     *      errorMsg: xx //错误提示信息 
-     *  }] 
-     * }])
-     *   if(errorMsg){
-     *     self.$vux.toast.text(errorMsg);
-     *     return;
-     *   }
-     */
+
 function validator() {
 
     let strategies, validataFunc;
 
     //设置策略对象 规则
     strategies = {
-        isNonEmpty: function (value, errorMsg) { //不为空
+        isNonEmpty: function (value, errorMsg) {
             if (value === '') {
                 return errorMsg;
             }
         },
-        minLength: function (value, length, errorMsg) { //限制最小长度
+        minLength: function (value, length, errorMsg) {
             if (value.length < length) {
                 return errorMsg;
             }
         },
-        maxLength: function (value, length, errorMsg) { //限制最大长度
+        maxLength: function (value, length, errorMsg) {
             if (value.length > length) {
                 return errorMsg;
             }
         },
-        isEqual: function (oldValue, newValue, errorMsg) { //匹配是否相等
+        isEqual: function (oldValue, newValue, errorMsg) {
             if (oldValue !== newValue) {
                 return errorMsg;
             }
-        }
+        },
+        isPhone: function (value, errorMsg) {
+            var reg = /^[1][3,4,5,7,8][0-9]{9}$/;
+            if (!reg.test(value)) {
+                return errorMsg;
+            }
+        },
+        isEmail: function (value, errorMsg) {
+            var reg = new RegExp("^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$");
+            if (!reg.test(value)) {
+                return errorMsg;
+            }
+        },
+
     };
 
-    //Validator类的实现
+
     var Validator = function () {
-        this.cache = []; //保存校验规则
+        this.cache = [];
     };
 
     Validator.prototype.add = function (value, rules) {
@@ -71,23 +64,21 @@ function validator() {
 
     Validator.prototype.start = function () {
         for (var i = 0, validatorFunc; validatorFunc = this.cache[i++];) {
-            var errorMsg = validatorFunc(); //开始校验，并取得校验后的返回信息
-            if (errorMsg) { //如果有确切的返回值，说明校验没有通过
+            var errorMsg = validatorFunc();
+            if (errorMsg) {
                 return errorMsg;
             }
         }
     };
 
-    //Context 用户调用和进行表单匹配
+
     validataFunc = function (options) {
         var validator = new Validator();
-
-        /***************添加一些校验规则***************/
         for (var i = 0, strategy; strategy = options[i++];) {
             validator.add(strategy.input, strategy.rules);
         }
-        var errorMsg = validator.start(); //获得校验结果
-        return errorMsg; //阻止表单提交
+        var errorMsg = validator.start();
+        return errorMsg;
     }
 
     return validataFunc;
